1d574801da0ebcd4e7eacec4360ea8839a511cb9
Jia Zhang <jiazhang@jias-mbp.media.mit.edu>
2014-03-11 18:12:40 -0400

    blah

--- Problem2/linegraph.js
+++ Problem2/linegraph.js
@@ -7,7 +7,7 @@
         top: 50,
         right: 50,
         bottom: 50,
-        left: 50
+        left: 150
     };
 
     width = 960 - margin.left - margin.right;
@@ -29,12 +29,60 @@
     }).append("g").attr({
             transform: "translate(" + margin.left + "," + margin.top + ")"
         });
+		
+	var line = d3.svg.line()
+	    .x(function(d) { return x(d.Year); })
+	    .y(function(d) { return y(d.Census); });
 
+	var x = d3.scale.linear()
+	    .range([0, width]);
 
-    d3.csv("timeline.csv", function(data) {
+	var y = d3.scale.linear()
+	    .range([height, 0]);
 
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left");
+			
+    d3.csv("data.csv", function(data) {
+		var censusData = []
         // convert your csv data and add it to dataSet
+		data.forEach(function(d){
+			d.Year = parseInt(d.Year);
+			d.Census = parseInt(d["United States Census Bureau (2009) [4 ]"])
+			if (!isNaN(d.Census)){
+				censusData.push(d)
+			}
+		})
+		console.log(censusData);
+		
+		x.domain(d3.extent(data, function(d) { return d.Year; }));
+		y.domain([0, d3.max(data, function(d) { return d.Census; })]);
 
+		  svg.append("g")
+		      .attr("class", "x axis")
+		      .attr("transform", "translate(0," + height + ")")
+		      .call(xAxis);
+
+		  svg.append("g")
+		      .attr("class", "y axis")
+		      .call(yAxis)
+		    .append("text")
+		      .attr("transform", "rotate(-90)")
+		      .attr("y", 6)
+		      .attr("dy", ".71em")
+		      .style("text-anchor", "end");
+
+		  svg.append("path")
+		      .datum(censusData)
+		      .attr("class", "line")
+		      .attr("d", line);
+		
+		
         return createVis();
     });
 
